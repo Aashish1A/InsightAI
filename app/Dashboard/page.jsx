c099ce858fc1d9f13bb9ee9b0a1dbb7aade2c69d
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2026-02-25 01:28:38 +0530

    refactor(DashboardPage): Implement dashboard data fetching and enhance layout

--- app/Dashboard/page.jsx
+++ app/Dashboard/page.jsx
@@ -1,6 +1,20 @@
+"use client";
+import { dummyCreationData } from "@/public/assets/asset";
+import { useEffect, useState } from "react";
+
 export default function DashboardPage() {
+    const [creation, setCreation] = useState([]);
+
+    const getDashboardData = async () => {
+        setCreation(dummyCreationData);
+    }
+
+    useEffect(() => {
+        getDashboardData();
+    }, []);
+
     return (
-        <div className="p-8">
+        <div className="h-full overflow-y-scroll p-6">
             <h1>Dashboard</h1>
         </div>
     );
