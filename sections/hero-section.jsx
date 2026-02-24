b619a6296d91ee2c18a23348b56d7ac5cfc29d2c
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2026-02-25 00:42:23 +0530

    feat(DashboardLayout, HeroSection): Integrate Clerk authentication and enhance user profile display

--- sections/hero-section.jsx
+++ sections/hero-section.jsx
@@ -10,12 +10,8 @@ export default function HeroSection() {
             <div className="max-w-7xl mx-auto flex flex-col items-center justify-center h-screen">
                 <AnimatedContent reverse distance={30} className="flex items-center gap-2 bg-white/50 backdrop-blur p-1 rounded-full">
                     <div className="flex items-center -space-x-3">
-                        <img className="size-7 rounded-full border-2 border-white"
-                            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=50"
-                            alt="userImage1" />
-                        <img className="size-7 rounded-full border-2 border-white"
-                            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=50"
-                            alt="userImage2" />
+                        <Image className="size-7 rounded-full border-2 border-white" src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=50" width={28} height={28} alt="userImage1" />
+                        <Image className="size-7 rounded-full border-2 border-white" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=50" width={28} height={28} alt="userImage2" />
                     </div>
                     <div className="h-5 w-px mx-1 bg-white rounded-full" />
                     <span>AI-Powered Resume Builder & Content Suite</span>
@@ -27,7 +23,7 @@ export default function HeroSection() {
                 </AnimatedContent>
                 <AnimatedContent distance={30} delay={0.1} className="relative">
                     <h1 className="text-center font-urbanist text-5xl/tight md:text-7xl/tight mt-4 font-extrabold max-w-4xl tracking-tight">
-                        Build Perfect Resumes <span className="text-4xl/tight md:text-6xl/tight font-bold bg-linear-to-r from-orange-600 to-orange-500 bg-clip-text text-transparent">& AI Content Instantly</span>
+                        Build Perfect Resumes <span className="text-5xl/tight md:text-7xl/tight font-extrabold bg-linear-to-r from-orange-600 to-orange-500 bg-clip-text text-transparent">& AI Content Instantly</span>
                     </h1>
                     <div className="absolute -top-5 right-13 hidden md:block">
                         <CustomIcon icon={SparkleIcon} dir="right" />
@@ -39,18 +35,13 @@ export default function HeroSection() {
                     </p>
                 </AnimatedContent>
                 <AnimatedContent className="flex flex-col md:flex-row items-center gap-4 mt-6 w-full md:w-auto">
-                    <Link href="/" className="py-3 md:py-2.5 w-full md:w-auto px-8 border border-orange-200 bg-linear-to-tl from-orange-600 to-orange-500 text-white text-center rounded-full">
+                    <Link href="/Dashboard" className="py-3 w-full md:w-auto px-8 border border-orange-200 bg-linear-to-tl from-orange-600 to-orange-500 text-white text-center rounded-full">
                        Build My Resume
                     </Link>
-                    <Link href="/" className="relative py-3 md:py-2.5 w-full md:w-auto px-8 bg-white/50 text-gray-600 font-medium text-center border border-white rounded-full">
+                    <Link href="/ATS-Checker" className="relative py-3 w-full md:w-auto px-8 bg-white/50 text-gray-600 font-medium text-center border border-white rounded-full">
                         Check ATS Score
                         <AnimatedContent direction="horizontal" className="absolute size-8 pointer-events-none right-0 top-full -translate-y-1/2">
-                            <Image
-                                src="/assets/mouse-arrow.svg"
-                                alt="mouse-arrow"
-                                width={24}
-                                height={24}
-                            />
+                            <Image src="/assets/mouse-arrow.svg" alt="mouse-arrow" width={24} height={24} />
                         </AnimatedContent>
                     </Link>
                 </AnimatedContent>
